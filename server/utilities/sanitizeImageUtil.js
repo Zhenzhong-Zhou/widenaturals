fb5f04eabf08a1396c732a56efbecd4f6ce2753a
Zhenzhong Zhou <bob0823.zhou@gmail.com>
2024-08-23 21:19:36 -0700

    Fix bugs

--- server/utilities/sanitizeImageUtil.js
+++ server/utilities/sanitizeImageUtil.js
@@ -1,6 +1,7 @@
 const sharp = require("sharp");
 const { promises: fs } = require("node:fs");
-const path = require("path");
+const {CustomError} = require("../middlewares/error/errorHandler");
+const logger = require("../utilities/logger");
 
 /**
  * Sanitizes an image by resizing, converting to JPEG format, and optionally creating a thumbnail.
@@ -18,8 +19,14 @@ const sanitizeImageFile = async (filePath, options = { width: 1024, quality: 80,
             .jpeg({ quality: options.quality }) // Adjust the quality
             .toBuffer();
         
-        // Determine the image type based on the processing format
-        const imageType = 'image/jpeg';  // Since we are converting to JPEG, set it to 'image/jpeg'
+        // Get stats to check the file size after processing
+        const stats = await sharp(buffer).metadata();
+        
+        // Throw error if image size is below the acceptable threshold
+        if (stats.size < 30720) {
+            logger.warn('Processed image size is below the acceptable threshold of 50KB.', { imageSize: stats.size });
+            throw new CustomError(400, 'Processed image size is below the acceptable threshold of 50KB.', { imageSize: stats.size });
+        }
         
         // Overwrite the file with the sanitized image asynchronously
         await fs.writeFile(filePath, buffer);
@@ -35,11 +42,14 @@ const sanitizeImageFile = async (filePath, options = { width: 1024, quality: 80,
                 .toFile(thumbnailPath);
         }
         
+        // Determine the image type based on the processing format
+        const imageType = 'image/jpeg';  // Since we are converting to JPEG, set it to 'image/jpeg'
+        
         // Return paths and metadata
         return { thumbnailPath, imageType };
         
     } catch (error) {
-        console.error(`Error sanitizing image: ${error.message}`);
+        logger.error(`Error sanitizing image: ${error.message}`);
         throw new Error(`Error sanitizing image: ${error.message}`);
     }
 };
